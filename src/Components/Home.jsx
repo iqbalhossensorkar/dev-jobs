f27dca422d70b738114545cb4c0426be79f5f315
iqbalhossensorkar <iqbalhossen303@gmail.com>
2023-04-10 06:31:04 +0600

    Footer added

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Header from './Header';
 import { Outlet } from 'react-router-dom';
+import Footer from './Footer';
 
 const Home = () => {
     return (
         <div>
-            <div>
-                <Header />
-            </div>
+            <Header />
             <Outlet />
+            <Footer></Footer>
         </div>
     );
 };
