01fc04ee026266085e1c1896fc613109b7d0fb95
iqbalhossensorkar <iqbalhossen303@gmail.com>
2023-04-12 06:08:27 +0600

    statistics page rechart added

--- src/Components/AppliedCard.jsx
+++ src/Components/AppliedCard.jsx
@@ -5,21 +5,21 @@ import { Link } from 'react-router-dom';
 const AppliedCard = ({ job }) => {
     const { company_logo, company_name, job_title, remote_or_onsite, fulltime_or_parttime, salary, location, id } = job;
     return (
-        <div>
-            <div className='p-8 border border-[#E8E8E8] rounded-lg mb-6 flex justify-between items-center'>
-                <div className='flex items-center '>
-                    <div className='px-8 py-14 bg-[#F4F4F4] rounded-lg mr-8'>
+        <>
+            <div className='p-8 border border-[#E8E8E8] rounded-lg mb-6 md:flex justify-between items-center'>
+                <div className='md:flex items-center'>
+                    <div className='px-8 py-14 bg-[#F4F4F4] rounded-lg'>
                         <img src={company_logo} alt="" className='h-14 w-24' />
                     </div>
-                    <div>
+                    <div className='text-left ml-6'>
                         <h3 className='mt-9 font-bold text-2xl text-[#474747]'>{job_title}</h3>
                         <p className='mt-2 text-[#757575] font-semibold'>{company_name}</p>
                         <div className='my-4'>
-                            <button className='text-gradient text-base font-bold px-5 py-2 border rounded-md mr-4 border-blue-500'>{remote_or_onsite}</button>
+                            <button className='text-gradient text-base font-bold px-5 py-2 border rounded-md mr-4 border-blue-500 mb-2'>{remote_or_onsite}</button>
                             <button className='text-gradient text-base font-bold px-5 py-2 border rounded-md mr-4 border-blue-500'>{fulltime_or_parttime}</button>
 
                         </div>
-                        <div className="flex justify-normal items-center mt-4 mb-6">
+                        <div className="md:flex justify-start items-center mt-4 mb-6">
                             <MapPinIcon fill='none' stroke='#474747' className="h-6 w-6 mr-2" />
                             <p className='mr-6 text-[#757575] font-semibold'>{location}</p>
                             <CurrencyDollarIcon fill='none' stroke='#474747' className="h-6 w-6 mr-2" />
@@ -30,7 +30,7 @@ const AppliedCard = ({ job }) => {
                 <Link className='btn-all' to={`/details/${id}`}>View Details</Link>
 
             </div>
-        </div>
+        </>
     );
 };
 
--- src/Components/Statistics.jsx
+++ src/Components/Statistics.jsx
@@ -1,15 +1,68 @@
 import React from 'react';
 import Loading from './loading';
 import { useNavigation } from 'react-router-dom';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Legend } from 'recharts';
+
+
 
 const Statistics = () => {
     const navigation = useNavigation();
     if (navigation.state === "loading") {
         return <Loading></Loading>
     }
+    const data = [
+        { id: 1, marks: 60, assignment: "A1" },
+        { id: 2, marks: 56, assignment: "A2" },
+        { id: 3, marks: 60, assignment: "A3" },
+        { id: 4, marks: 58, assignment: "A4" },
+        { id: 5, marks: 57, assignment: "A5" },
+        { id: 6, marks: 53, assignment: "A6" },
+        { id: 7, marks: 60, assignment: "A7" },
+        { id: 8, marks: 60, assignment: "A8" }
+    ];
     return (
-        <div className='min-h-screen'>
-            <h1>Statistics</h1>
+        <div className=''>
+
+            <div className='container mx-auto mt-32 mb-8'>
+                {/* <ResponsiveContainer height={400}>
+                    <LineChart width={1000} height={300} data={marksArray}>
+                        <Line stroke="#8884d8" dataKey="physics" activeDot={{ r: 8 }}></Line>
+                        <Line dataKey="chemistry" stroke="#82ca9d" />
+                        <Line dataKey="math" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                    </LineChart>
+                </ResponsiveContainer> */}
+                <div style={{ width: '100%', height: 400 }}>
+                    <ResponsiveContainer>
+                        <AreaChart
+                            data={data}
+                            margin={{
+                                top: 10,
+                                right: 30,
+                                left: 0,
+                                bottom: 0,
+                            }}
+                        >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="assignment" />
+                            <defs>
+                                <linearGradient id="colorAssignment" x1="0" y1="0" x2="0" y2="1">
+                                    <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                                    <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                                </linearGradient>
+                            </defs>
+
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                            <Area type="monotone" dataKey="marks" stroke="#82ca9d" fillOpacity={1} fill="url(#colorAssignment)" />
+                        </AreaChart>
+                    </ResponsiveContainer>
+                </div>
+            </div>
         </div>
     );
 };
